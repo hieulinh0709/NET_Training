ea149640f688644c840704f9a7c4962bb6875a13
waiting <hieulinh512@gmail.com>
2021-08-19 23:36:44 +0700

    delegate

---
+++ CSharpBasic/Delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDelegate testdelegate = new TestDelegate();
+            //Instantiation
+            TestDelegate.FirstDelegate fd1 = new TestDelegate.FirstDelegate(testdelegate.fun1);
+            TestDelegate.FirstDelegate fd2 = new TestDelegate.FirstDelegate(testdelegate.fun2);
+            TestDelegate.FirstDelegate fd3 = new TestDelegate.FirstDelegate(testdelegate.fun3);
+
+            //Invocation 
+            fd1();
+            fd2();
+            fd3();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpBasic/Delegate/TestDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Delegate
+{
+    public class TestDelegate
+    {
+        //Declaration
+        //Creating Delegates with no parameters and no return type.
+        public delegate void FirstDelegate();
+
+        public void fun1()
+        {
+            Console.WriteLine("I am Function 1");
+        }
+        public void fun2()
+        {
+            Console.WriteLine("I am Function 2");
+        }
+        public void fun3()
+        {
+            Console.WriteLine("I am Function 3");
+        }
+    }
+}

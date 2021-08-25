0259f18fcba22ba9ef9a388458fae1c26c96a86c
waiting <hieulinh512@gmail.com>
2021-08-25 09:42:26 +0700

    clean

--- CheckNumberPhone/UnitTests/PhoneRepositoryTest.cs
+++ CheckNumberPhone/UnitTests/PhoneRepositoryTest.cs
@@ -5,6 +5,8 @@ using FindBeeNumbers.Core.Entities;
 using FindBeeNumbers.Core.Model;
 using FindBeeNumbers.Repository.IRepository;
 using FindBeeNumbers.Repository.Repository;
+using FindBeeNumbers.Service;
+using FindBeeNumbers.Service.Interface;
 using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -19,6 +21,7 @@ namespace UnitTests
     {
         #region field
         private readonly PhoneContext _phoneContext;
+        private readonly IPhoneService _phoneService;
         private readonly IPhoneRepository _phoneRepository;
         public static IConfiguration _configuration;
         #endregion field
@@ -31,6 +34,7 @@ namespace UnitTests
                     .Build();
             _phoneContext = new PhoneContext(_configuration);
             _phoneRepository = new PhoneRepository(_phoneContext);
+            _phoneService = new PhoneService(_phoneRepository);
         }
 
         [TestMethod]
@@ -40,73 +44,7 @@ namespace UnitTests
             Assert.IsNotNull(phones);
         }
 
-        [TestMethod]
-        public void ListNumberPhoneChecked()
-        {
-            List<Phone> phones = new List<Phone>();
-            List<Phone> phonesChecked = new List<Phone>();
-            Bee bee = new Bee();
-            ReadOnlyJson readOnlyJson = new ReadOnlyJson();
-            string fileName = @"\Bee.json";
-
-            // Gets the full path or UNC location of the loaded file that contains the manifest.
-            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + fileName;
-
-            phones = _phoneRepository.GetPhones();
-            bee = readOnlyJson.ReadJson(path);
-            var providerNetworkFromBee = bee.Providers;
-            var twoLastNumberTabooFromBee = bee.TabooNumbers;
-            var twoLastNumberNiceFromBee = bee.NiceNumbers;
-
-            if (phones == null)
-            {
-                Assert.IsNull(phones);
-            }
-            else
-            {
-                Assert.IsNotNull(phones);
-                foreach (var phone in phones)
-                {
-                    var twoLastNumber = phone.Number.Substring(phone.Number.Length - 2);
-
-                    // Check provider network
-                    bool checkProviderNetwork = Program.IsProviderNetwork(phone, providerNetworkFromBee);
-                    if (checkProviderNetwork)
-                    {
-                        Assert.IsTrue(checkProviderNetwork);
-                        // The last 2 num chars is taboo
-                        bool isInTaboo = Array.Exists(twoLastNumberTabooFromBee, element => element.Equals(twoLastNumber));
-                        if (isInTaboo)
-                        {
-                            Assert.IsTrue(isInTaboo);
-                            // rule is violated
-                            //Console.WriteLine("rule is violated => number phone: {0} - {1} rule is violated,", phone.Number, phone.Network);
-                        }
-                        else
-                        {
-                            Assert.IsFalse(isInTaboo);
-                            // Total first 5 nums / Total last 5 nums: matches 1 in n conditions configuratin into Bee.json
-                            bool checkTotal5NumberFirstAndLast = Program.IsTotal5NumberFirstAndLast(phone.Number, bee.SumOfNumbers);
-                            if (checkTotal5NumberFirstAndLast &&
-                                Array.Exists(twoLastNumberNiceFromBee, element => element.Equals(twoLastNumber)))
-                            {
-                                Assert.IsTrue(checkTotal5NumberFirstAndLast &&
-                                    Array.Exists(twoLastNumberNiceFromBee, element => element.Equals(twoLastNumber)));
-
-                                phonesChecked.Add(phone);
-                            };
-                        }
-                    }
-                    else
-                    {
-                        Assert.IsFalse(checkProviderNetwork);
-                    }
-
-
-                }
-            }
-
-        }
+        
 
     }
 }
